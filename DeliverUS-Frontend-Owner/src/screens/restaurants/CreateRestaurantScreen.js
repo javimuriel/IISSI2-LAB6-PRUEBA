aecfc458684da24bb7c5f70a98d659e61b591d5e
javimuriel <158613576+javimuriel@users.noreply.github.com>
2025-07-03 17:13:13 +0200

    Initial commit

---
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import { Image, Platform, Pressable, ScrollView, StyleSheet, View } from 'react-native'
+import * as ExpoImagePicker from 'expo-image-picker'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import * as yup from 'yup'
+import DropDownPicker from 'react-native-dropdown-picker'
+import { create, getRestaurantCategories } from '../../api/RestaurantEndpoints'
+import InputItem from '../../components/InputItem'
+import TextRegular from '../../components/TextRegular'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
+import restaurantBackground from '../../../assets/restaurantBackground.jpeg'
+import { showMessage } from 'react-native-flash-message'
+import { ErrorMessage, Formik } from 'formik'
+import TextError from '../../components/TextError'
+
+export default function CreateRestaurantScreen({ navigation }) {
+
+
+
+}
